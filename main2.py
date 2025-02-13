789bdc5d29e9624896b6b1fa60e37a4035003408
MAPiryazev <m.a.piryazev@gmail.com>
2025-02-13 09:19:01 +0300

    added functions

--- main2.py
+++ main2.py
@@ -1,6 +1,6 @@
 from os import access
 
-import streamlit
+import streamlit as st
 import pandas as pd
 from fastapi.openapi.models import OAuth2
 from streamlit import session_state
@@ -10,6 +10,7 @@ from jose import jwt, JWTError
 import bcrypt
 from sympy.codegen.ast import continue_
 from datetime import timedelta, datetime, timezone
+import requests
 
 from passw import hash_password, verify_password
 import services.users
@@ -23,6 +24,7 @@ app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 registr = services.regist.Registration()
 auth = Authotize()
+API_URL="http://127.0.0.1:8000"
 
 
 @app.post("/register")
@@ -35,7 +37,7 @@ def register_user(email: str, password: str):
 
 
 @app.post("/token") #это одновременно логин и выдача токена
-def login (email: str, password: str):
+def login_api (email: str, password: str):
     if auth.auth(email,password):
         access_token_expires = timedelta(minutes=settings.JWT_CONFIG["ACCESS_TOKEN_EXPIRE_MINUTES"])
         access_token = jwt.encode(
@@ -58,8 +60,44 @@ async def get_current_user(token: str= Depends(oauth2_scheme)):
     user = services.user.get_user(email)
     if user is None:
         raise credentials_exception
-    return user
+    return user.to_dict(orient="records")[0]
 
 @app.get("/profile")
-def get_profile(user: dict = Depends(get_current_user)): #TODO тут надо еще в дикт преобразовать нормально датафрейм
+def get_profile(user: dict = Depends(get_current_user)):
     return user
+
+
+
+def login_page():
+    st.title("Авторизация")
+    st.write("Введите почту и пароль")
+    email = st.text_input("Почта")
+    password = st.text_input("Пароль", type = "password")
+
+    if st.button("Войти"):
+        response = requests.post(f"{API_URL}/token", params={"email": email, "password": password})
+        if response.status_code == 200:
+            data = response.json()
+            st.session_state["token"] = data["access_token"]
+            st.session_state["authenticated"] = True
+            st.success("Вы успешно вошли!")
+        else:
+            st.error("Неверный email или пароль")
+
+
+
+def profile():
+    st.title("Профиль")
+
+    if "token" not in st.session_state:
+        st.error("Сначала войдите в систему")
+        return
+
+    headers = {"Authorization": f"Bearer {st.session_state['token']}"}
+    response = requests.get(f"{API_URL}/profile", headers=headers)
+
+    if response.status_code == 200:
+        user = response.json()
+        st.write(f"Ваш email: {user['email']}")
+    else:
+        st.error("Ошибка при получении профиля")
